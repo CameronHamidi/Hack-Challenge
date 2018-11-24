314ae7afb61faf371959d7a7520aa8cf5b3b4c84
Cameron Hamidi <cameronhamidi@Camerons-iMac.local>
2018-11-23 23:55:16 -0500

    finished skills selection for project search

--- Hack Challenge/ProjectSearchViewController.swift
+++ Hack Challenge/ProjectSearchViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ProjectSearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class ProjectSearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UITextFieldDelegate {
 
-    var rolesCollectionView: UICollectionView!
-    var roles = ["Developer", "Designer", "Backend Developer", "Frontend Developer"]
-    let NUM_ROLES_DEFAULT = 4
-    var selectedRoles: [String] = []
+    var skillsLabel: UILabel!
+    var skillsCollectionView: UICollectionView!
+    var skills = [String]()
+    var skillsTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,33 +23,59 @@ class ProjectSearchViewController: UIViewController, UICollectionViewDataSource,
         
         view.backgroundColor = .white
         
+        skillsLabel = UILabel()
+        skillsLabel.text = "Skills"
+        skillsLabel.font = UIFont.boldSystemFont(ofSize: 17.0)
+        skillsLabel.textAlignment = .left
+        skillsLabel.textColor = .black
+        skillsLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(skillsLabel)
+        
         var rolesLayout = UICollectionViewFlowLayout()
         rolesLayout.scrollDirection = .horizontal
         rolesLayout.minimumLineSpacing = 4
         rolesLayout.minimumInteritemSpacing = 4
-        rolesLayout.sectionInset = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        rolesLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         //rolesLayout.estimatedItemSize = CGSize(width: 50, height: 25)
+        skillsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: rolesLayout)
+        skillsCollectionView.delegate = self
+        skillsCollectionView.dataSource = self
+        skillsCollectionView.register(SkillsCollectionViewCell.self, forCellWithReuseIdentifier: "skill")
+        skillsCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(skillsCollectionView)
+        skillsCollectionView.reloadData()
+        skillsCollectionView.backgroundColor = .white
+        skillsCollectionView.allowsMultipleSelection = true
         
-        rolesCollectionView = UICollectionView(frame: .zero, collectionViewLayout: rolesLayout)
-//        rolesCollectionView.backgroundColor = .white
-        rolesCollectionView.delegate = self
-        rolesCollectionView.dataSource = self
-        rolesCollectionView.register(RolesCollectionViewCell.self, forCellWithReuseIdentifier: "role")
-        rolesCollectionView.register(CustomEnterCollectionViewCell.self, forCellWithReuseIdentifier: "custom")
-        rolesCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(rolesCollectionView)
-        rolesCollectionView.reloadData()
-        rolesCollectionView.backgroundColor = .lightGray
-        rolesCollectionView.allowsMultipleSelection = true
+        skillsTextField = UITextField()
+        skillsTextField.placeholder = " ex. Python"
+        skillsTextField.textColor = .black
+        skillsTextField.font = UIFont.systemFont(ofSize: 15.0)
+        skillsTextField.borderStyle = .roundedRect
+        skillsTextField.textColor = .gray
+        skillsTextField.delegate = self
+        skillsTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(skillsTextField)
         
         
         setupConstraints()
         
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == skillsTextField {
+            skills.append("X " + textField.text!)
+            skillsCollectionView.reloadData()
+            textField.text = ""
+            return true
+        } else {
+            return false
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == rolesCollectionView {
-            return roles.count + 1
+        if collectionView == skillsCollectionView {
+            return skills.count
         } else {
             return 0
         }
@@ -60,35 +86,16 @@ class ProjectSearchViewController: UIViewController, UICollectionViewDataSource,
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if collectionView == rolesCollectionView {
-            if selectedRoles.count == 0 {
-                if indexPath.row == NUM_ROLES_DEFAULT - 1 {
-                    
-                }
-                selectedRoles.append(roles[indexPath.row])
-            } else {
-                if selectedRoles.contains(roles[indexPath.row]) {
-                    for i in 0..<selectedRoles.count {
-                        if selectedRoles[i] == roles[indexPath.row] {
-                            selectedRoles.remove(at: i)
-                        }
-                    }
-                } else {
-                    selectedRoles.append(roles[indexPath.row])
-                }
-            }
+        if collectionView == skillsCollectionView {
+            skills.remove(at: indexPath.row)
         }
-       // collectionView.reloadData()
+        collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == self.rolesCollectionView {
-            if indexPath.row == NUM_ROLES_DEFAULT {
-                var cell = rolesCollectionView.dequeueReusableCell(withReuseIdentifier: "custom", for: indexPath) as! CustomEnterCollectionViewCell
-                cell.customTextField.placeholder = "Enter custom role"
-            }
-            var cell = rolesCollectionView.dequeueReusableCell(withReuseIdentifier: "role", for: indexPath) as! RolesCollectionViewCell
-            cell.configure(roleName: roles[indexPath.row])
+        if collectionView == self.skillsCollectionView {
+            var cell = skillsCollectionView.dequeueReusableCell(withReuseIdentifier: "skill", for: indexPath) as! SkillsCollectionViewCell
+            cell.configure(roleName: skills[indexPath.row])
             return cell
         } else {
             return UICollectionViewCell()
@@ -96,15 +103,11 @@ class ProjectSearchViewController: UIViewController, UICollectionViewDataSource,
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if collectionView == self.rolesCollectionView {
-            if indexPath.row == NUM_ROLES_DEFAULT {
-                return CGSize(width: 200.0, height: 15.0)
-            } else {
-                print("test: \(collectionView == rolesCollectionView)")
-                var width = ceil((Double)(roles[indexPath.row].count) / 10.0 * 75.0)
-                print(width)
-                return CGSize(width: width, height: 15.0)
-            }
+        if collectionView == self.skillsCollectionView {
+            print("test: \(collectionView == skillsCollectionView)")
+            var width = ceil((Double)(skills[indexPath.row].count) / 10.0 * 75.0 + 10.0)
+            print(width)
+            return CGSize(width: width, height: 25.0)
         } else {
             return CGSize()
         }
@@ -112,11 +115,24 @@ class ProjectSearchViewController: UIViewController, UICollectionViewDataSource,
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            rolesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            rolesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            rolesCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-            rolesCollectionView.heightAnchor.constraint(equalToConstant: 30)
+            skillsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            skillsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            skillsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            ])
+        
+        NSLayoutConstraint.activate([
+            skillsCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            skillsCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            skillsCollectionView.topAnchor.constraint(equalTo: skillsLabel.bottomAnchor, constant: 8),
+            skillsCollectionView.heightAnchor.constraint(equalToConstant: 25),
+
         ])
+        
+        NSLayoutConstraint.activate([
+            skillsTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            skillsTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            skillsTextField.topAnchor.constraint(equalTo: skillsCollectionView.bottomAnchor, constant: 8)
+            ])
     }
     
 
